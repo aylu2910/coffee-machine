ff47b6da0ef17e043185efd3d1b83aa4a96659a5
Aylén Orellana <ayluorellana@gmail.com>
2024-11-28 14:52:34 +0100

    add project structure

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("What would you like? (espresso/latte/cappuccino):")
+
+#todo: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):​”
+#todo: a.  Check the user’s input to decide what to do next.
+#todo: b.  The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
+#todo: 2.  Turn off the Coffee Machine by entering “off​” to the prompt.
+#todo: a.  For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. Your code should end execution when this happens.
+#todo: 3.  Print report.
+#todo: a.  When the user enters “report” to the prompt, a report should be generated that shows the current resource values. e.g.
+#todo: Water: 100ml 
+#todo: Milk: 50ml 
+#todo: Coffee: 76g 
+#todo: Money: $2.5 
+#todo: 4.  Check resources sufficient?
+#todo: a.  When the user chooses a drink, the program should check if there are enough resources to make that drink.
+#todo: b.  E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make the drink but print: “Sorry there is not enough water.​”
+#todo: c.   The same should happen if another resource is depleted, e.g. milk or coffee.
+#todo: 5.  Process coins.
+#todo: a.  If there are sufficient resources to make the drink selected, then the program should prompt the user to insert coins.
+#todo: b.  Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+#todo: c.   Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+#todo: pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+#todo: 6.  Check transaction successful?
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
